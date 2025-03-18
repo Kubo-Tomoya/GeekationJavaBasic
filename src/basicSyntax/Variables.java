dda1629b4dbad4f1a8a9a375d61a0d7a7f55accb
久保友哉 <t.kubo.cingroup2012@gmail.com>
2025-03-19 01:56:10 +0900

    Java基礎構文編３変数の課題を実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,21 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int num1 = 106;
+		double num2 =1.06;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		String str1="私の好きな整数は";
+		String str2="です";
+		String str3="私の好きな小数は";
+		
+		System.out.println(str1+num1+str2);
+		System.out.println(str3+num2+str2);
+		}
+
+}
